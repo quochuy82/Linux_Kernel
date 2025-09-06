7ac6b2a374fd99761771f1c80759c87a966f49aa
quochuy82 <hongkhanhngo82@gmail.com>
2025-09-06 09:22:32 -0700

    sua loi thoi gian

--- b2_Files/filestat.c
+++ b2_Files/filestat.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <time.h>
 
+char time_buf[64];
+struct tm *tm_info = NULL;
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
@@ -23,7 +26,13 @@ int main(int argc, char *argv[]) {
     printf("File Path     : %s\n", argv[1]);
     printf("File Type     : %s\n", type);
     printf("Size          : %lld bytes\n", (long long)st.st_size);
-    printf("Last Modified : %s", ctime(&st.st_mtime));
+    tm_info = localtime(&st.st_mtime);
+    if (tm_info) {
+        strftime(time_buf, sizeof time_buf, "%Y-%m-%d %H:%M:%S", tm_info);
+        printf("Last Modified : %s\n", time_buf);
+    } else {
+        printf("Last Modified : (invalid)\n");
+    }
 
     return 0;
 }
